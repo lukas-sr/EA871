0551d0f84b05c1514e615065007660b51cece7ee
lukas-sr <lukas-sr@hotmail.com>
2019-10-13 01:07:18 -0300

    Atividade 06

--- At06/TemplateAVR-master/src/main.c
+++ At06/TemplateAVR-master/src/main.c
@@ -9,34 +9,59 @@ volatile unsigned char *p_ucsrb;
 volatile unsigned char *p_ucsrc;
 volatile unsigned char *p_ubrrh;
 volatile unsigned char *p_ubrrl;
+volatile unsigned char *p_udr0;
 
 setup(){
-	*p_ucsra = (*unsigned char) 0xC0;
-	*p_ucsrb = (*unsigned char) 0xC1;
-	*p_ucsrc = (*unsigned char) 0xC2;
-	*p_ubrrh = (*unsigned char) 0xC4;
-	*p_ubrrh = (*unsigned char) 0xC5;
+	p_ucsra = (unsigned char *) 0xC0;
+	p_ucsrb = (unsigned char *) 0xC1;
+	p_ucsrc = (unsigned char *) 0xC2;
+	p_ubrrh = (unsigned char *) 0xC4;
+	p_ubrrh = (unsigned char *) 0xC5;
+	p_udr0 = (unsigned char *) 0xC6;
 
-	*p_ucsra &= 0xFC; //0xFC = 0b1111_1100
-	*p_ucsrb &= 0x08;
-	*p_ucsrc &= 0x36;
-	*p_ubrrh &= 
+	/*
+	 * Modo de transmissão multi-processador desabilitado e velocidade de transmissão normal: UCSRA[1:0] setados como 0
+	 * Todas as interrupções relacionadas ao UART desabilitadas: UCSR0B[7:5] setados como 0
+	 * Somente o transmissor ativado: UCSR0B[3] setado como 1 e UCSR0B[4] setado como 0
+	 * Número de bits de dados por frame igual a 8: UCZ0n[2:0] = (011) -> Valores setados em UCSR0B(bit 2) em UCSR0C(bits 1 e 2)
+	 * Modo Assíncrono de funcionamento da USART: UCSR0C[7:6] setados como 00(UMSEL = 00) e UCSR0C[0] setado como 0
+	 * Uso de um bit de paridade (ímpar): UCSR0C[5:4] setados como 11 (UPM = 11)
+	 * Uso de um bit apenas de parada: UCSR0C[3] setado como 0 (USBS0 = 0)
+	 */
+	*p_ucsra |= 0x00;
+	*p_ucsrb |= 0x08;
+	*p_ucsrc |= 0x36;
+	
+	//Setando o baundrate como 38400, UBRRn[11:0] precisa ser setado como 25
+	*p_ubrrh = 0;
+	*p_ubrrl = 25; 
 }
 
-int main (){
-	setup();
+void le_strings(char vet[]){
 
 	int i;
-	char msg = " Out of the night that covers me,\n Black as the Pit from pole to pole,\n I thank
-	whatever gods may be\n For my unconquerable soul.\n In the fell clutch of circumstance\n I have not
-	winced nor cried aloud.\n Under the bludgeonings of chance\n My head is bloody, but unbowed.\n
-	Beyond this place of wrath and tears\n Looms but the Horror of the shade,\n And yet the menace of
-	the years\n Finds, and shall find, me unafraid.\n It matters not how strait the gate,\n How charged
-	with punishments the scroll.\n I am the master of my fate:\n I am the captain of my soul.\n";
+
+	for( i=0 ; vet[i]!='\0' ;){
+		*p_udr0 = vet[i];
+
+		if(*p_ucsra & 0b00100000){
+			i++;
+			_delay_ms(100);
+		}
+	}
+
+}
+int main (){
+
+	setup();
+
+	char msg[] = "»Esta é a coisa que tudo devora\n»Feras,aves, plantas, flora.\n»Aço e ferro são sua comida,\n»E a dura pedra por ele moída;\n»Aos reis abate,a cidade arruína\n»E a alta montanha faz pequenina.";
+	char end[] = "\n»Mensagem transmitida com sucesso...";
 
 	while(1){
-		for(i=0;i<strlen(msg);i++){
-			printf("%s",msg[i]);
-		}
+		
+		le_strings(msg);
+		le_strings(end);
+		_delay_ms(5000);
 	}
 }
